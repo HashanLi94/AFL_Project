1d19813b994348ca2f4af88415daadba4bb8fe98
Hashan Liyanage <55444962+HashanLi94@users.noreply.github.com>
2020-08-19 11:36:23 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {LogInComponent} from './components/log-in/log-in.component';
+import {PassengerSelectionComponent} from './components/event-selection/passenger-selection/passenger-selection.component';
+import {TicketSelectionDetailsComponent} from './components/ticket-selection/ticket-selection-detials/ticket-selection-details.component';
+import {ExtrasSelectionComponent} from './components/extras-selection/extras-selection.component';
+import {NotFoundComponent} from './components/not-found/not-found.component';
+import {RodLaverAreanaComponent} from './components/ticket-selection/rod-laver-areana/rod-laver-areana.component';
+import {SearchTripDetailsComponent} from './components/search-trip-details-only/search-trip-details/search-trip-details.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: LogInComponent},
+  {path: 'passengerDetails', component: PassengerSelectionComponent},
+  {path: 'ticketSelection', component: TicketSelectionDetailsComponent},
+  {path: 'rod-laver-arena', component: RodLaverAreanaComponent},
+  {path: 'extras-selection', component: ExtrasSelectionComponent},
+  {path: 'not-found', component: NotFoundComponent},
+  {path: 'search-tripDetails', component: SearchTripDetailsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,85 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from './components/app/app.component';
+import { LogInComponent } from './components/log-in/log-in.component';
+import { PassengerSelectionComponent } from './components/event-selection/passenger-selection/passenger-selection.component';
+import {
+  AccordionModule, BreadcrumbModule,
+  ButtonModule,
+  CalendarModule, CardModule,
+  ChartModule, CheckboxModule, DropdownModule,
+  InputNumberModule,
+  InputTextModule,
+  MenuModule,
+  MessageModule, PanelModule, RadioButtonModule, ToastModule
+} from 'primeng';
+import {HttpClientModule} from '@angular/common/http';
+import {RouterModule} from '@angular/router';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { TicketSelectionDetailsComponent } from './components/ticket-selection/ticket-selection-detials/ticket-selection-details.component';
+import { RodLaverAreanaComponent } from './components/ticket-selection/rod-laver-areana/rod-laver-areana.component';
+import { ExtrasSelectionComponent } from './components/extras-selection/extras-selection.component';
+import {NotFoundComponent} from './components/not-found/not-found.component';
+import { SearchTripDetailsComponent } from './components/search-trip-details-only/search-trip-details/search-trip-details.component';
+import { SummerySearchTripDetailsComponent } from './components/search-trip-details-only/summery-search-trip-details/summery-search-trip-details.component';
+import { FlightsHotelsComponent } from './components/search-trip-details-only/flights-hotels/flights-hotels.component';
+import { FlightsOnlyComponent } from './components/search-trip-details-only/flights-only/flights-only.component';
+import { HotelsOnlyComponent } from './components/search-trip-details-only/hotels-only/hotels-only.component';
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LogInComponent,
+    PassengerSelectionComponent,
+    NavbarComponent,
+    TicketSelectionDetailsComponent,
+    RodLaverAreanaComponent,
+    ExtrasSelectionComponent,
+    SearchTripDetailsComponent,
+    SummerySearchTripDetailsComponent,
+    FlightsHotelsComponent,
+    FlightsOnlyComponent,
+    HotelsOnlyComponent,
+    NotFoundComponent
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AccordionModule,
+    MenuModule,
+    InputTextModule,
+    ChartModule,
+    ButtonModule,
+    HttpClientModule,
+    RouterModule,
+    ReactiveFormsModule,
+    ButtonModule,
+    BrowserAnimationsModule,
+    InputNumberModule,
+    FormsModule,
+    CalendarModule,
+    MessageModule,
+    CardModule,
+    ToastModule,
+    PanelModule,
+    DropdownModule,
+    BreadcrumbModule,
+    CheckboxModule,
+    AppRoutingModule,
+    RadioButtonModule
+
   ],
-  providers: [],
+  providers: [
+    PassengerSelectionComponent,
+    TicketSelectionDetailsComponent
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
